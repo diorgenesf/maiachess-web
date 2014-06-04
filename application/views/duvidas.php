c72ec44883ad9da25d73a63a4827b06305d1e6ac
renatocesar1 <renatocesar_olm@hotmail.com>
2014-06-03 23:23:30 -0300

    Tela Regras
    É NOIS!

--- application/views/duvidas.php
+++ application/views/duvidas.php
@@ -1,42 +1,43 @@
 <div id="Duvidas">
-	<div class="container16 box-transparent-light">
+	<div class="container16"> 
+		<div class="box-transparent-light box">
 
-		<div class="row">			
-			<div class="SubTituloContent">
-				<div class="column6">
-					<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
-				</div>
+			<div class="row">			
+				<div class="SubTituloContent">
+					<div class="column6">
+						<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
+					</div>
 				
-				<h3 class="column4">Dúvidas</h3> 
+					<h3 class="column4">Dúvidas</h3> 
 				
-				<div class="column6">
-					<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
-				</div>
-			</div>
-		</div>
-		
-		<div class="seila"><h4>-Onde estou?</h4><h4>-Estou cadastrado?</h4><h4>-Como desafiar alguém?</h4><h4>-Como desativar minha conta?</h4></div>
-		
-		<div class="row">			
-			<div class="SubTituloContent">
-				<div class="column6">
-					<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
-				</div>
-				
-				<h4 class="column4">Enviar dúvida</h4> 
-				
-				<div class="column6">
-					<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
+					<div class="column6">
+						<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
+					</div>
 				</div>
 			</div>
-		</div>
+		
+			<div class="conteudo"><h4>-Onde estou?</h4><br /><h4>-Estou cadastrado?</h4><br /><h4>-Como desafiar alguém?</h4><br /><h4>-Como desativar minha conta?</h4></div>
+		
+			<div class="row">			
+				<div class="SubTituloContent">
+					<div class="column6">
+						<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
+					</div>
+				
+					<h4 class="column4">Enviar dúvida</h4> 
+				
+					<div class="column6">
+						<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
+					</div>
+				</div>
+			</div>
 
-		<div class="row">
-			<div class="column6">
-				<img src="<?php echo base_url(); ?>public/imagens/imgduvidas.png"/> 
-			</div>
-			<div class="column10">
-				<form name="formDuvidas" class="Duvidas"  method="post">
+			<div class="row">
+				<div class="column6">
+					<img src="<?php echo base_url(); ?>public/imagens/imgFinal.png"/> 
+				</div>
+				<div class="column10">
+					<form name="formDuvidas" class="Duvidas"  method="post">
 						
 						<div class="row">
 							<label>Nome</label>
@@ -59,8 +60,9 @@
 						<div class="row">
 							<input type="submit" class="btn" value="Enviar"/>
 						</div> 
-				</form>
-			</div>
-        </div>
+					</form>
+				</div>
+        	</div>
+		</div>
 	</div>
 </div>
--- application/views/regras.php
+++ application/views/regras.php
@@ -1,7 +1,58 @@
-<div class="bg-logado">
-<div class="container16">
-<div class="row">Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!! E que abundância meu irmão viuu!! Assim você vai matar o papai. Só digo uma coisa, Domingo ela não vai! Danadaa!! Vem minha odalisca, agora faz essa cobra coral subir!!! Pau que nasce torto, Nunca se endireita. Tchannn!! Tchannn!! Tu du du pááá! Eu gostchu muitchu, heinn! danadinha! Mainhaa! Agora use meu lorem ipsum ordinária!!! Olha o quibeee! rema, rema, ordinária!.</div>
-<div class="row">Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!! E que abundância meu irmão viuu!! Assim você vai matar o papai. Só digo uma coisa, Domingo ela não vai! Danadaa!! Vem minha odalisca, agora faz essa cobra coral subir!!! Pau que nasce torto, Nunca se endireita. Tchannn!! Tchannn!! Tu du du pááá! Eu gostchu muitchu, heinn! danadinha! Mainhaa! Agora use meu lorem ipsum ordinária!!! Olha o quibeee! rema, rema, ordinária!.</div>
-<div class="row">Eiiitaaa Mainhaaa!! Esse Lorem ipsum é só na sacanageeem!! E que abundância meu irmão viuu!! Assim você vai matar o papai. Só digo uma coisa, Domingo ela não vai! Danadaa!! Vem minha odalisca, agora faz essa cobra coral subir!!! Pau que nasce torto, Nunca se endireita. Tchannn!! Tchannn!! Tu du du pááá! Eu gostchu muitchu, heinn! danadinha! Mainhaa! Agora use meu lorem ipsum ordinária!!! Olha o quibeee! rema, rema, ordinária!.</div>
-</div>
+<div id="Regras">
+	<div class="container16"> 
+		<div class="box-transparent-light box">
+
+			<div class="row">			
+				<div class="SubTituloContent">
+					<div class="column6">
+						<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
+					</div>
+				
+					<h3 class="column4">Regras</h3> 
+				
+					<div class="column6">
+						<img src="<?php echo base_url(); ?>public/imagens/linha.gif"/> 
+					</div>
+				</div>
+			</div>
+
+			<div class="conteudo">
+				<img src="<?php echo base_url(); ?>public/imagens/pecasTorre.png"/>
+				<h3>Torre</h3><br />
+				<h4>A torre se movimenta para frente e para trás, para a direita e para a esquerda, quantas casas quiser, mas não pode pular nenhuma outra peça.</h4>
+				<br />
+
+				<img src="<?php echo base_url(); ?>public/imagens/pecasBispo.png"/>
+				<h3>Bispo</h3><br />
+				<h4>O bispo se movimenta na diagonal mantendo-se sempre nas casas de mesma cor que se encontrava no início do jogo, podendo ir para frente e para trás, quantas casas quiser, mas não pode pular nenhuma outra peça.</h4>
+				<br />
+
+				<img src="<?php echo base_url(); ?>public/imagens/pecasCavalo.png"/>
+				<h3>Cavalo</h3><br />
+				<h4>O cavalo tem um movimento especial que parece a letra L. O cavalo se movimenta 2 casas para frente ou para trás e em seguida 1 casa para a direita ou para a esquerda, ou 2 casas para a direita ou para a esquerda e em seguida 1 casa para frente ou para trás. O cavalo é a única peça do xadrez que pode pular outras peças.</h4>
+				<br />
+
+				<img src="<?php echo base_url(); ?>public/imagens/pecasRainha.png"/>
+				<h3>Rainha</h3><br />
+				<h4>A Rainha, também conhecida como Dama, é a peça mais poderosa do xadrez, ela pode ir para frente ou para trás, para direita ou para a esquerda, ou na diagonal, quantas casas quiser, mas não pode pular nenhuma outra peça.</h4>
+				<br />
+
+				<img src="<?php echo base_url(); ?>public/imagens/pecasRei.png"/>
+				<h3>Rei</h3><br />
+				<h4>O Rei movimenta-se apenas 1 casa em qualquer direção. O Rei nunca pode se movimentar para uma casa que esteja sob ataque ou capturar uma peça que esteja defendida por uma peça adversária. No diagrama o rei preto só pode ir para cima, pois indo para a esquerda ou em diagonal estará sob ataque da torre branca. Quando estudarmos os movimentos especiais veremos que existe uma situação em que o rei pode andar 2 casas.</h4>
+				<br />
+
+				<img src="<?php echo base_url(); ?>public/imagens/pecasPeao.png"/>
+				<h3>Peão</h3><br />
+				<h4>O peão só se movimenta para frente, sendo a única peça que não se move para trás. No primeiro lance de cada peão ele pode avançar 1 ou 2 casas. À partir do segundo lance de cada peão ele irá movimenta-se apenas 1 casa.</h4>
+				<br />
+
+				<img src="<?php echo base_url(); ?>public/imagens/tabuleiro.gif"/>
+				<h3>Tabuleiro</h3><br />
+				<h4>As peças são distribuídas no tabuleiro de acordo com o desenho acima.<br/>As brancas sempre fazem o primeiro movimento!</h4>
+
+
+			</div>
+		</div>
+	</div>
 </div>
